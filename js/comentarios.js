05d7fdbf2860a38cb88ef0c6083d5da75a351adc
Claudio Henrique <claudio_henrique20@hotmail.com>
2023-03-05 19:14:33 -0300

    style: comments container

--- js/comentarios.js
+++ js/comentarios.js
@@ -26,47 +26,15 @@ async function getAllPosts() {
   const data = await response.json();
   loadingElement.classList.add("hide");
 
-  // data.map((post) => {
   for (let i = 0; i < 5; i++) {
     const post = data[i];
     const div = document.createElement("div");
-    // const title = document.createElement("h3");
-    // const body = document.createElement("p");
-    // const image = document.createElement("img");
-    // const link = document.createElement("a");
-    // const separar = document.createElement("hr");
-
-    // title.innerText = post.title;
-    // body.innerText = post.body;
-    // link.innerText = "Comentar post";
-    // link.setAttribute("href", `./enviar-comentario06.html?id=${post.id}`);
-
-    // div.appendChild(title);
-    // div.appendChild(body);
-    // div.appendChild(image);
-    // div.appendChild(link);
-    // div.appendChild(separar);
     div.id = `post-${post.id}`;
 
-    div.innerHTML = `<section class="text-black p-3 mb-5 rounded shadow" style="max-width: 800px; margin: 10px auto; background: white;">
-        <div class="d-flex align-items-center">
-            <h5 class="">${post.title}</h5>
-            <h4>${post.body}</h4>
-        </div>
-        <div class="d-flex justify-content-between align-items-center mt-3">
-            <p class="">Data de envio: <span style="font-weight: bold;">03/03/2023</span></p>
-            <p class="">Avaliação: <span class="post-${post.id} rating">
-              ${starIcon}
-              ${starIcon}
-              ${starIcon}
-              ${starIcon}
-              ${starIcon}
-            </span></p>
-        </div>
-    </section>`;
+    div.innerHTML = createCommentSection(post.id, post.title, post.body);
+
     postsContainer.appendChild(div);
   }
-  // });
 }
 getAllPosts();
 
@@ -99,33 +67,37 @@ async function getPost(id) {
 
 function createComment(comment) {
   const div = document.createElement("div");
-  // const email = document.createElement("h3");
-  // const commentBody = document.createElement("p");
 
-  // email.innerText = comment.email;
-  // commentBody.innerText = comment.body;
+  div.innerHTML = createCommentSection(comment.id, comment.email, comment.body);
 
-  // div.appendChild(email);
-  // div.appendChild(commentBody);
-  div.innerHTML = `<section class="text-black p-3 mb-5 rounded shadow" style="max-width: 800px; margin: 10px auto; background: white;">
-        <div class="d-flex align-items-center">
-            <h5 class="">${comment.email}</h5>
-            <h4>${comment.body}</h4>
-        </div>
-        <div class="d-flex justify-content-between align-items-center mt-3">
-            <p class="">Data de envio: <span style="font-weight: bold;">03/03/2023</span></p>
-            <p class="">Avaliação: <span class="post-comment rating">
-              ${starIcon}
-              ${starIcon}
-              ${starIcon}
-              ${starIcon}
-              ${starIcon}
-            </span></p>
-        </div>
-    </section>`;
   commentsContainer.appendChild(div);
   //Salvar o comentário no localStorage em um array de comentários e ao carregar a página, verificar se existe algum comentário salvo no localStorage e se existir, carregar os comentários salvos no localStorage
 }
+
+const createCommentSection = (id, email, body) => {
+  return `<section class="comment">
+        <div class="comment-header">
+            <h4>${email}</h4>
+            <p>
+              Data de envio:
+              <small>03/03/2023</small>
+            </p>
+            <p>
+              Avaliação:
+              <span class="post-${id} rating">
+              ${starIcon}
+              ${starIcon}
+              ${starIcon}
+              ${starIcon}
+              ${starIcon}
+              </span>
+            </p>
+        </div>
+        <div class="comment-content">
+            <p>${body}</p>
+        </div>
+    </section>`;
+};
 
 async function postComment(comment) {
   const response = await fetch(url, {
